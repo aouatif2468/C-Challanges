a22636dbb8cbca7ec28317964e103afd6c95f3f2
Aouatif Cherdid <awatifcherdid2468@gmail.com>
2025-09-26 17:59:58 +0200

    Add Mini projet

--- Tableaux/Mini Projet .c
+++ Tableaux/Mini Projet .c
@@ -10,7 +10,8 @@ int main(){
        int quantite[10];
        int nbLivre = 0;
        char titreRecherche[10];
-        int i;
+       int i;
+       int S=0;
 
        
        do{
@@ -72,23 +73,69 @@ switch(Choix){
             printf("  Prix  : %d\n", prix[i]);
             printf("  Quantite: %d\n\n", quantite[i]);
         } else {
-            printf("Livre non trouvé\n");
+              
+            printf("Livre non trouve ! \n");
         }
     break;
-     case 4:
-     break;
-     case 5:
-     break;
-     case 6:
-     break;
-     case 0:
-       printf("Au revoir !\n");
-       break;
-            default:
-                break;
+     case 4: 
+            printf("Entrez le titre du livre a mettre a jour : ");
+            scanf("%s", titreRecherche);
+            i = 0;
+            while (i < nbLivre && strcmp(titreRecherche, titre[i]) != 0) {
+                i++;
+            }
+            if (i < nbLivre) {
+                printf("Ancienne quantite : %d\n", quantite[i]);
+                printf("Nouvelle quantite : ");
+                scanf("%d", &quantite[i]);
+                printf("Quantite mise a jour avec succes.\n");
+            } else {
+                printf("Livre non trouve !\n");
+            }
+            break;
+
+        case 5: 
+            printf("Entrez le titre du livre a supprimer : ");
+            scanf("%s", titreRecherche);
+            i = 0;
+            while (i < nbLivre && strcmp(titreRecherche, titre[i]) != 0) {
+                i++;
+            }
+            if (i < nbLivre) {
+                for (int j = i; j < nbLivre - 1; j++) {
+                    strcpy(titre[j], titre[j + 1]);
+                    strcpy(auteur[j], auteur[j + 1]);
+                    prix[j] = prix[j + 1];
+                    quantite[j] = quantite[j + 1];
+                }
+                nbLivre--;
+                printf("Livre supprime avec succes.\n");
+            } else {
+                printf("Livre non trouve !\n");
+            }
+            break;
+
+        case 6: 
+            if (nbLivre == 0) {
+                printf("Aucun livre en stock.\n");
+            } else {
+                int total = 0;
+                for (i = 0; i < nbLivre; i++) {
+                    total =total + quantite[i];
+                }
+                printf("Nombre total de livres en stock : %d\n", total);
+            }
+            break;
+
+        case 0:
+            printf("Au revoir !\n");
+            break;
+
+        default:
+            break;
         }
 
-}while(Choix != 0);
+    } while (Choix != 0);
 
-return 0;
+    return 0;
 }
